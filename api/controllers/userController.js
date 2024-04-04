ec519747a7e394286bd41f2369a1d02e184e2f14
R4him <148691250+BigBr41n@users.noreply.github.com>
2024-04-04 12:38:49 +0200

    registration operation

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,8 +1,40 @@
+const User = require('../models/user'); 
+const HttpError = require('../models/errorModel'); 
+const bcrypt = require('bcrypt'); 
+
 //========= REGISTER NEW USER
 //========= POST : /api/users/register
 //========= UNPROTECTED
-module.exports.registerUser = (req , res)=>{
+module.exports.registerUser = async (req , res ,next)=>{
+    try {
+        const {username , email , password} = req.body ; 
+        if (!username  || !email || !password){
+            return next(new HttpError("Fill in all Fields." , 422)); 
+        }
+        //lower case email 
+        const newEmail = email.toLowerCase(); 
 
+        //cheking if the user exist 
+        const userE = await User.findOne({email : newEmail}); 
+        if(userE){
+            return next(new HttpError('email already exists.',422)); 
+        }
+        //checking the length of the pass 
+        if((password.trim()).length < 6){
+            return next(new HttpError('password should be at least 6 chars', 422)); 
+        }
+
+        //hashing the pass 
+        const hash = await bcrypt.hash(password , 10); 
+
+        //creating the user 
+        const newUser = await User.create({username : username , email : newEmail , password : hash}); 
+        res.status(200).json(newUser); 
+
+    } catch (error) {
+        console.log(error); 
+        return next(new HttpError("User registration field." , 422)); 
+    }
 }
 
 
