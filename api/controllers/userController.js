a0afbb9f7aacdf61893114b217cfe19497b489e5
R4him <148691250+BigBr41n@users.noreply.github.com>
2024-04-04 11:08:57 +0200

    user controllers

---
+++ api/controllers/userController.js
@@ -0,0 +1,55 @@
+//========= REGISTER NEW USER
+//========= POST : /api/users/register
+//========= UNPROTECTED
+module.exports.registerUser = (req , res)=>{
+
+}
+
+
+
+
+//========= LOGIN REGISTRED USER
+//========= POST : /api/users/login
+//========= UNPROTECTED
+module.exports.loginUser = (req , res)=>{
+
+}
+
+
+
+
+//========= USER PROFILE
+//========= GET : /api/users/:id
+//========= PROTECTED
+module.exports.getUser = (req , res)=>{
+
+}
+
+
+
+
+//========= CHANGE USER AVATER
+//========= POST : /api/users/change-avatar
+//========= PROTECTED
+module.exports.changeAvatar = (req , res)=>{
+
+}
+
+
+
+
+//========= EDIT USER DETAILS
+//========= POST : /api/users/edit-user
+//========= PROTECTED
+module.exports.editUser = (req , res)=>{
+
+}
+
+
+
+//========= GET AUTHORS
+//========= GET : /api/users/authors
+//========= UNPROTECTED
+module.exports.getAuthors = (req , res)=>{
+
+}
