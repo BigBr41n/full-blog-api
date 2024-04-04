1a8ede4202a889881979205e81009f60bec0b527
R4him <148691250+BigBr41n@users.noreply.github.com>
2024-04-04 12:55:25 +0200

    login functionality

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,6 +1,8 @@
 const User = require('../models/user'); 
 const HttpError = require('../models/errorModel'); 
 const bcrypt = require('bcrypt'); 
+const jwt  = require ('jsonwebtoken'); 
+require('dotenv').config(); 
 
 //========= REGISTER NEW USER
 //========= POST : /api/users/register
@@ -29,7 +31,7 @@ module.exports.registerUser = async (req , res ,next)=>{
 
         //creating the user 
         const newUser = await User.create({username : username , email : newEmail , password : hash}); 
-        res.status(200).json(newUser); 
+        res.status(200).json({message : "user registred perfectly"}); 
 
     } catch (error) {
         console.log(error); 
@@ -43,8 +45,40 @@ module.exports.registerUser = async (req , res ,next)=>{
 //========= LOGIN REGISTRED USER
 //========= POST : /api/users/login
 //========= UNPROTECTED
-module.exports.loginUser = (req , res)=>{
+module.exports.loginUser = async (req , res , next)=>{
+    try {
+        const {email , password} =req.body ; 
+        if (!email || !password){
+            return next(new HttpError('Fill in Fields to login ' , 422)); 
+        }
 
+        //lower case the email 
+        const newEmail = email.toLowerCase(); 
+        
+        //finding the user 
+        const user  = await User.findOne({email : newEmail}); 
+
+        if (!user) {
+            return next(new HttpError('account does not exists.', 422)); 
+        }
+
+        //verifying the pass 
+        const compared = await bcrypt.compare(password , user.password) ;
+        if(!compared){
+            return next(new HttpError('Invalid credentials' , 422)); 
+        }
+
+
+        //sending token 
+        const {_id : id , username} = user ; 
+        const token = jwt.sign({id , username} , process.env.JWT , {expiresIn : "3d"});
+        res.status(200).json({token , id , username}); 
+
+
+    } catch (error) {
+        console.log(error)
+        return next(new HttpError('check your Credentials please' , 422)); 
+    }
 }
 
 
